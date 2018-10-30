0fba5d8397360dfac1696f047efce234229a322b
allanquiman <43649727+allanquiman@users.noreply.github.com>
2018-10-30 14:05:56 -0300

    Update sirb.sql
    cotejamiento latin a utf-8

--- sirb.sql
+++ sirb.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 4.8.3
+-- version 4.8.1
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 29-10-2018 a las 22:50:59
--- Versión del servidor: 10.1.36-MariaDB
--- Versión de PHP: 7.2.10
+-- Tiempo de generación: 30-10-2018 a las 18:03:30
+-- Versión del servidor: 10.1.33-MariaDB
+-- Versión de PHP: 7.2.6
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET AUTOCOMMIT = 0;
@@ -19,7 +19,7 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Base de datos: `sirb`
+-- Base de datos: `sirb_rem`
 --
 
 -- --------------------------------------------------------
@@ -29,10 +29,10 @@ SET time_zone = "+00:00";
 --
 
 CREATE TABLE `departamentos` (
-  `Nombre_Dep` varchar(30) NOT NULL,
-  `Descripcion` varchar(50) NOT NULL,
-  `Titular` text NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+  `Nombre_Dep` varchar(30) COLLATE utf8_spanish_ci NOT NULL,
+  `Descripcion` varchar(50) COLLATE utf8_spanish_ci NOT NULL,
+  `Titular` text COLLATE utf8_spanish_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
 
 -- --------------------------------------------------------
 
@@ -41,17 +41,17 @@ CREATE TABLE `departamentos` (
 --
 
 CREATE TABLE `empleado` (
-  `Nombre` text NOT NULL,
-  `Apellido_Pat` text NOT NULL,
-  `Apellido_Mat` text NOT NULL,
-  `Cargo` text NOT NULL,
-  `Direccion` varchar(30) NOT NULL,
-  `Ciudad` text NOT NULL,
+  `Nombre` text COLLATE utf8_spanish_ci NOT NULL,
+  `Apellido_Pat` text COLLATE utf8_spanish_ci NOT NULL,
+  `Apellido_Mat` text COLLATE utf8_spanish_ci NOT NULL,
+  `Cargo` text COLLATE utf8_spanish_ci NOT NULL,
+  `Direccion` varchar(30) COLLATE utf8_spanish_ci NOT NULL,
+  `Ciudad` text COLLATE utf8_spanish_ci NOT NULL,
   `Telefono` int(9) NOT NULL,
   `Fecha_Nac` date NOT NULL,
   `Salario` int(20) NOT NULL,
-  `RUT_TRABAJADOR` varchar(10) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+  `RUT_TRABAJADOR` varchar(10) COLLATE utf8_spanish_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
 
 -- --------------------------------------------------------
 
@@ -61,15 +61,15 @@ CREATE TABLE `empleado` (
 
 CREATE TABLE `empresa` (
   `RUT` int(10) NOT NULL,
-  `Razon_social` varchar(30) NOT NULL,
-  `Direccion` varchar(50) NOT NULL,
-  `Giro` text NOT NULL,
+  `Razon_social` varchar(30) COLLATE utf8_spanish_ci NOT NULL,
+  `Direccion` varchar(50) COLLATE utf8_spanish_ci NOT NULL,
+  `Giro` text COLLATE utf8_spanish_ci NOT NULL,
   `Telefono` int(9) NOT NULL,
-  `FAX` varchar(10) NOT NULL,
-  `Email` varchar(50) NOT NULL,
-  `Representante` varchar(40) NOT NULL,
+  `FAX` varchar(10) COLLATE utf8_spanish_ci NOT NULL,
+  `Email` varchar(50) COLLATE utf8_spanish_ci NOT NULL,
+  `Representante` varchar(40) COLLATE utf8_spanish_ci NOT NULL,
   `RUN_Representante` int(10) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
 
 -- --------------------------------------------------------
 
@@ -78,10 +78,10 @@ CREATE TABLE `empresa` (
 --
 
 CREATE TABLE `usuarios` (
-  `Usuario` varchar(20) NOT NULL,
-  `Contraseña` varchar(8) NOT NULL,
+  `Usuario` varchar(20) COLLATE utf8_spanish_ci NOT NULL,
+  `Contraseña` varchar(8) COLLATE utf8_spanish_ci NOT NULL,
   `Privilegios` int(1) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
 
 --
 -- Volcado de datos para la tabla `usuarios`
