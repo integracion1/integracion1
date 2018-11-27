b7ab007e8ea00cd8a501e1f1ff6ce7398d133aa4
YoungEagleII <43649910+YoungEagleII@users.noreply.github.com>
2018-11-27 15:50:38 -0300

    Actualizacion
    Actualizacion

--- informatica/Busqueda.php
+++ informatica/Busqueda.php
@@ -6,16 +6,22 @@
 
     include ("conect.inc");
 
-    $Cons =    "SELECT Nombre, RUT_TRABAJADOR, Departamento 
-                FROM personal 
+    $Cons =    "SELECT Nombre, Apellidos, RUT_TRABAJADOR, Departamento, Fecha_Nac, Direccion, Ciudad, Comuna, Telefono
+                FROM personal
                 WHERE Nombre = '$Usuario' AND RUT_TRABAJADOR = '$RUT' AND Departamento = '$Depa'";
     $resp = mysqli_query($db, $Cons);
 
     echo "<h1 style='font-family:Helvetica; font-size:26px; text-indent:2cm'>Información Disponible</h1>";
     while ($fila=mysqli_fetch_object($resp)) {
-        echo "Nombre: '$fila->Nombre' <br>";
-        echo "Run: '$fila->RUT_TRABAJADOR'<br>";
-        echo "Departamento: '$fila->Departamento'";
+        echo "Nombres: '$fila->Nombre' <br><br>";
+        echo "Apellidos: '$fila->Apellidos' <br><br>";
+        echo "Run: '$fila->RUT_TRABAJADOR'<br><br>";
+        echo "Departamento: '$fila->Departamento'<br><br>";
+        echo "Fecha Nacimiento: '$fila->Fecha_Nac' <br><br>";
+        echo "Direccion: '$fila->Direccion' <br><br>";
+        echo "Ciudad: '$fila->Ciudad' <br><br>";
+        echo "Comuna: '$fila->Comuna' <br><br>";
+        echo "Telefono: '$fila->Telefono' <br><br>";
     }
 
 ?>
