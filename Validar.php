3ae1ea694def688997ea978ef0e5760d31477f6e
pablinho17 <ptapia2018@alu.uct.cl>
2018-12-17 23:13:21 -0300

    Login terminado

---
+++ Validar.php
@@ -0,0 +1,39 @@
+<?php
+
+session_start();
+
+$user = $_GET["user"];
+$passw = $_GET["pass"];
+
+//supongamos que consultamos estos datos en la BD
+while(true) {
+
+if ($user=="Gerente" && $passw=="gerente") {
+	$_SESSION['estado'] = "logeado";
+$_SESSION['nombre'] = "Sr. Administrador";
+header("Location: Gerente y modelo/index.html");  
+	
+}
+else {
+header("Location: inicio.php?error=Usuario+o+contraseña+inválida");  //redirecciona al inicio enviando un msj de error como parámetro
+}
+if ($user=="RRHH" && $passw=="recursos") {
+	$_SESSION['estado'] = "logeadoR";
+$_SESSION['nombre'] = "Sr. Administrador";
+header("Location: RRHH/index.html");  
+	
+}
+else {
+header("Location: inicio.php?error=Usuario+o+contraseña+inválida");  //redirecciona al inicio enviando un msj de error como parámetro
+}
+	if ($user=="Juridico" && $passw=="juridico") {
+	$_SESSION['estado'] = "logeadoR";
+$_SESSION['nombre'] = "Sr. Administrador";
+header("Location: Juridico/index.html ");  
+	
+}
+else {
+header("Location: inicio.php?error=Usuario+o+contraseña+inválida");  //redirecciona al inicio enviando un msj de error como parámetro
+}
+}
+?>
---
+++ inicio.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="es">
+  <head>
+    <meta charset="utf-8" />
+   <title>Entrar - SIRB(Acceder)</title>
+   <link rel="stylesheet" type="text/css" href="estilos.css">
+  </head>
+
+<body>
+  <h1>S.I.R.B.</h1>
+  <h2>Sistema Informático de Remuneraciones Basico</h2>
+  <form action="Validar.php" id="formlg">
+    <h2 class="form_titulo">Ingrese Nombre de Usuario</h2>
+    <div class="inputdetabla">
+  Usuario   <input type="text" name="user"  /> <br />
+  Contraseña<input type="password" name="pass" /> <br />
+            <input type="submit" />
+            </div>
+<?php
+    $error = $_GET['error'];
+    echo $error;
+?>	
+  </form>
+</body>
+
+</html>
+
+
+
